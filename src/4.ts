106684bf3d1ec416079a78d01687378bf15a5ea3
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2023-12-15 23:01:33 +0200

    Домашка

--- src/4.ts
+++ src/4.ts
@@ -1,5 +1,47 @@
-const key = new Key();
+class Key{
+  constructor(private signature: number = Math.floor(Math.random() * 99) + 1) {
+  }
+  getSignature(): number {
+      return this.signature; 
+  }
+}
 
+
+class Person {
+  constructor(private key:Key=key){
+  }
+  getKey(){
+    return this.key;
+  }
+}
+
+
+
+abstract class House{
+  protected door: boolean= false;
+  protected tenants: Person[] = [];
+  constructor(protected key: Key=key){
+
+  }
+  comeIn(tenant: Person): void {
+    if (this.door) this.tenants.push(tenant); 
+  }
+  abstract openDoor(key: Key): void;
+  
+}
+
+
+
+class MyHouse extends House{
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) this.door = true; 
+  }
+
+}
+
+
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
